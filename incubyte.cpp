816df17f55d095a6990a5c8287537049cd5ef64b
Aditya_Magdum <magdumadiraj@gmail.com>
2023-09-15 13:48:08 +0530

    leftright commit

--- incubyte.cpp
+++ incubyte.cpp
@@ -37,6 +37,36 @@ class Chandrayan
             else if(face=='D') z++;
         }
 
+        void left()
+        {
+            if(face=='N') face='W';
+            else if(face=='W') face='S';
+            else if(face=='S') face='E';
+            else if(face=='E') face='N';
+            else if(face=='U' or face=='D')
+            {
+                if(prev=='N') face='W';
+                else if(prev=='W') face='S';
+                else if(prev=='S') face='E';
+                else if(prev=='E') face='N';
+            } 
+        }
+
+        void right()
+        {
+            if(face=='N') face='E';
+            else if(face=='W') face='N';
+            else if(face=='S') face='W';
+            else if(face=='E') face='S';
+            else if(face=='U' or face=='D')
+            {
+                if(prev=='N') face='E';
+                else if(prev=='W') face='N';
+                else if(prev=='S') face='W';
+                else if(prev=='E') face='S';
+            } 
+        }
+
         void printPosition()
         {
             cout<<"Current Position: ("<<x<<","<<y<<","<<z<<") Direction:"<<face<<endl;
@@ -46,13 +76,15 @@ class Chandrayan
 int main()
 {
     int x1=0,y1=0,z1=0;
-    char face1='W';
+    char face1='N';
     Chandrayan c(x1,y1,z1,face1);
-    char a[3]={'f','b','b'};
+    char a[3]={'f','l','b'};
     for(int i=0;i<3;i++)
     {
         if(a[i]=='f') c.forward();
-        else c.backward();
+        else if(a[i]=='b') c.backward();
+        else if(a[i]=='l') c.left();
+        else if(a[i]=='r') c.right();
     }
     c.printPosition();
     return 0;
